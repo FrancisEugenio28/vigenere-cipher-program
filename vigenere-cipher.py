ee702079ecf005b4665be01361ac6b8c0ae1198b
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-16 21:27:09 +0800

    Pseudocode

---
+++ vigenere-cipher.py
@@ -0,0 +1,12 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Program 3
+
+# Create an input function for message and for the key
+# Repeat the length of the key base on the length of the Message
+# Create an empty set wherein we will put the output into it
+# We will scan the characters in the message using a for loop 
+# Gather the ASCII code both message and the key
+# Perform the formulation of Vigenere Cipher Algorithm
+# Gather the Cipher text with the ASCII code then return it to the empty set 
+# print the empty set or the output
