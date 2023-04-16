7683173b25c69012bf6f147ec1e6de0706fee9f1
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-16 21:32:17 +0800

    Done coding wherein I repeat the length of the key base on the length of the message

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -6,6 +6,9 @@
 message = input("Please enter your Message: ")
 key = input("Please enter your key: ")
 # Repeat the length of the key base on the length of the Message
+def vigenere_cipher(message,key):
+    key_index = key * (len(message)// len(key) + 1)
+    key_index = key_index[:len(message)]
 # Create an empty set wherein we will put the output into it
 # We will scan the characters in the message using a for loop 
 # Gather the ASCII code both message and the key
